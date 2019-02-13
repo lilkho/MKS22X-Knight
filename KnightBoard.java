5976caed4c8ef70c47c2f2f8218bb2d1a79ad59f
lilkho <mkhondker@stuy.edu>
2019-02-13 14:20:24 -0500

    empty methods

---
+++ KnightBoard.java
@@ -0,0 +1,15 @@
+public class KnightBoard {
+  public KnightBoard(int startingRows,int startingCols) {
+
+  }
+  public boolean solve(int startingRow, int startingCol) {
+    return true;
+  }
+  private boolean solveH(int row ,int col, int level)  {
+    return true;
+  }
+  public int countSolutions(int startingRow, int startingCol) {
+    return 0;
+  }
+
+}
