1bdb038dd793e349f10d012aaba48ed339832704
lilkho <mkhondker@stuy.edu>
2019-02-13 14:33:14 -0500

    constructor nd tostring

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,13 +1,28 @@
 public class KnightBoard {
-  public KnightBoard(int startingRows,int startingCols) {
+  private int[][] board;
 
+  public KnightBoard(int startingRows,int startingCols) {
+    board = new int[startingRows][startingCols];
   }
+
+  public String toString() {
+    String s = "";
+    for (int i=0;i<board.length;i++) {
+      for (int j=0;j<board.length;j++) {
+        if (board[i][j]==0) s+=" _";
+        else s+= " "+board[i][j];
+      }
+    }
+  }
+
   public boolean solve(int startingRow, int startingCol) {
-    return true;
+    return solveH(0,0,1);
   }
   private boolean solveH(int row ,int col, int level)  {
-    return true;
+    if (level==row*col) return true;
+    for
   }
+
   public int countSolutions(int startingRow, int startingCol) {
     return 0;
   }
