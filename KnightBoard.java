70a62764d6dc324bb8d4da14de1ecc554931141a
lilkho <mkhondker@stuy.edu>
2019-02-26 19:32:27 -0500

    adjusting solve method, fixed many errors

--- KnightBoard.java
+++ KnightBoard.java
@@ -22,22 +22,24 @@ public class KnightBoard {
     return solveH(startingRow,startingCol,1);
   }
   private boolean solveH(int row ,int col, int level)  {
-    if (board[row][col]==0) {
-      board[row][col]=level;
-    }
-    if (level==row*col) return true;
-    for (int i=row;i<board.length;i++) {
-      for (int j=col;j<board[i].length;j++) {
+    if (level==board.length*board[0].length) return true;
+    for (int i=0;i<board.length;i++) {
+      for (int j=0;j<board[i].length;j++) {
         int[] moves = {2,1,2,-1,-2,1,-2,-1,1,2,1,-2,-1,2,-1,-2};
         for (int k=0;k<moves.length;k+=2) {
           System.out.println(Text.go(1,1));
           System.out.println(this);Text.wait(50); //adjust this delay
-          if (i+k>=0 && j+k<board.length
-          && j+moves[k+1]>=0 && i+moves[k+1]<board[0].length) {
-            if (solveH(i+k,j+moves[k+1],level+1)) {
+          if (row+moves[k]>=0 && row+moves[k]<board.length
+          && col+moves[k+1]>=0 && col+moves[k+1]<board[0].length) {
+            if (board[row][col]==0) {
+              board[row][col]=level;
+            }
+            level++;
+            if (solveH(row+moves[k],col+moves[k+1],level)) {
               return true;
             }
-            clearLevels(level);
+            board[row+moves[k]][col+moves[k]]=0;
+            level--;
           }
         }
       }
