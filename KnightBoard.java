b2718a8b1b7d8eee872be075870783e5f90dd5d3
lilkho <mkhondker@stuy.edu>
2019-02-26 20:45:47 -0500

    countsolutions is done

--- KnightBoard.java
+++ KnightBoard.java
@@ -41,7 +41,25 @@ public class KnightBoard {
   }
 
   public int countSolutions(int startingRow, int startingCol) {
-    return 0;
+    clearLevels(0);
+    return countSolutionsH(0,0,1);
+  }
+  public int countSolutionsH(int row, int col, int level) {
+    int solutions = 0;
+    board[row][col]=level;
+    if (level==board.length*board[0].length) solutions++;
+    int[] moves = {2,1,2,-1,-2,1,-2,-1,1,2,1,-2,-1,2,-1,-2};
+    for (int k=0;k<moves.length;k+=2) {
+      //System.out.println(Text.go(1,1));
+      //System.out.println(this);Text.wait(1); //adjust this delay
+      if (row+moves[k]>=0 && row+moves[k]<board.length
+      && col+moves[k+1]>=0 && col+moves[k+1]<board[0].length
+      && board[row+moves[k]][col+moves[k+1]]==0) {
+        solutions+=countSolutionsH(row+moves[k],col+moves[k+1],level+1);
+        board[row+moves[k]][col+moves[k+1]]=0;
+      }
+    }
+    return solutions;
   }
 
   private void clearLevels(int level) {
@@ -54,8 +72,16 @@ public class KnightBoard {
 
   public static void main(String[] args) {
     KnightBoard board = new KnightBoard(5,5);
-    //System.out.println(board);
     System.out.println(board.solve(0,0));
+    System.out.println(board.countSolutions(0,0));
+    System.out.println(board);
+    board = new KnightBoard(5,6);
+    System.out.println(board.solve(0,0));
+    System.out.println(board.countSolutions(0,0));
+    System.out.println(board);
+    board = new KnightBoard(6,6);
+    System.out.println(board.solve(0,0));
+    System.out.println(board.countSolutions(0,0));
     System.out.println(board);
   }
 }
