33deb957a6074cf9cab86919f3d04107545c86db
lilkho <mkhondker@stuy.edu>
2019-02-26 20:53:27 -0500

    adding exceptions

--- KnightBoard.java
+++ KnightBoard.java
@@ -2,6 +2,7 @@ public class KnightBoard {
   private int[][] board;
 
   public KnightBoard(int startingRows,int startingCols) {
+    if (startingRows<=0 || startingCols<=0) throw new IllegalArgumentException();
     board = new int[startingRows][startingCols];
   }
 
@@ -19,6 +20,15 @@ public class KnightBoard {
 
   public boolean solve(int startingRow, int startingCol) {
     clearLevels(0);
+    for (int i=0;i<board.length;i++) {
+      for (int j=0;j<board[i].length;j++) {
+        if (board[i][j]!=0) throw new IllegalStateException();
+      }
+    }
+    if (startingRow<0 || startingCol<0
+    || startingRow>=board.length || startingCol>=board[0].length) {
+      throw new IllegalArgumentException();
+    }
     return solveH(startingRow,startingCol,1);
   }
   private boolean solveH(int row ,int col, int level)  {
@@ -42,6 +52,15 @@ public class KnightBoard {
 
   public int countSolutions(int startingRow, int startingCol) {
     clearLevels(0);
+    for (int i=0;i<board.length;i++) {
+      for (int j=0;j<board[i].length;j++) {
+        if (board[i][j]!=0) throw new IllegalStateException();
+      }
+    }
+    if (startingRow<0 || startingCol<0
+    || startingRow>=board.length || startingCol>=board[0].length) {
+      throw new IllegalArgumentException();
+    }
     return countSolutionsH(0,0,1);
   }
   public int countSolutionsH(int row, int col, int level) {
