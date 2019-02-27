dac081f02fc42c219036e76cd959ba5663a12b3e
lilkho <mkhondker@stuy.edu>
2019-02-26 20:32:40 -0500

    dumb mistakes that hindered my success but i got it now

--- KnightBoard.java
+++ KnightBoard.java
@@ -8,7 +8,7 @@ public class KnightBoard {
   public String toString() {
     String s = "";
     for (int i=0;i<board.length;i++) {
-      for (int j=0;j<board.length;j++) {
+      for (int j=0;j<board[i].length;j++) {
         if (board[i][j]==0) s+=" _";
         else s+= " "+board[i][j];
       }
@@ -22,22 +22,19 @@ public class KnightBoard {
     return solveH(startingRow,startingCol,1);
   }
   private boolean solveH(int row ,int col, int level)  {
+    board[row][col]=level;
     if (level==board.length*board[0].length) return true;
     int[] moves = {2,1,2,-1,-2,1,-2,-1,1,2,1,-2,-1,2,-1,-2};
     for (int k=0;k<moves.length;k+=2) {
-      System.out.println(Text.go(1,1));
-      System.out.println(this);Text.wait(50); //adjust this delay
+      //System.out.println(Text.go(1,1));
+      //System.out.println(this);Text.wait(1); //adjust this delay
       if (row+moves[k]>=0 && row+moves[k]<board.length
-        && col+moves[k+1]>=0 && col+moves[k+1]<board[0].length) {
-          if (board[row][col]==0) {
-            board[row][col]=level;
-          }
-          level++;
-          if (solveH(row+moves[k],col+moves[k+1],level)) {
-            return true;
-          }
-          board[row+moves[k]][col+moves[k]]=0;
-          level--;
+      && col+moves[k+1]>=0 && col+moves[k+1]<board[0].length
+      && board[row+moves[k]][col+moves[k+1]]==0) {
+        if (solveH(row+moves[k],col+moves[k+1],level+1)) {
+          return true;
+        }
+        board[row+moves[k]][col+moves[k+1]]=0;
       }
     }
     return false;
@@ -56,7 +53,7 @@ public class KnightBoard {
   }
 
   public static void main(String[] args) {
-    KnightBoard board = new KnightBoard(5,6);
+    KnightBoard board = new KnightBoard(5,5);
     //System.out.println(board);
     System.out.println(board.solve(0,0));
     System.out.println(board);
